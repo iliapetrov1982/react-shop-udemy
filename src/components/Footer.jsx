80cf934befaa9aea6f44e17e06b1c2ac36f0ea82
ILIA PETROV <petrovilia8@gmail.com>
2023-01-22 18:03:42 +0100

    add link to Footer.jsx and Header.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,13 @@ function Footer() {
         <footer className="page-footer light-blue darken-1">
             <div className="container">
                 © {new Date().getFullYear()} Copyright Text
-                <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+                <a
+                    className="grey-text text-lighten-4 right"
+                    href="https://github.com/iliapetrov1982/react-shop-udemy/settings"
+                    target="_blank"
+                    rel="noreferrer"
+                >Repo
+                </a>
             </div>
         </footer>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,16 @@
 function Header() {
     return <nav className="light-blue darken-2">
         <div className="nav-wrapper">
-            <a href="#" className="brand-logo">React Shop</a>
+            <a href="/" className="brand-logo">React Shop</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="#">Repo</a></li>
+                <li>
+                    <a
+                        href="https://github.com/iliapetrov1982/react-shop-udemy/settings" target="_blank"
+                        rel="noreferrer"
+                    >
+                        Repo
+                    </a>
+                </li>
             </ul>
         </div>
     </nav>
